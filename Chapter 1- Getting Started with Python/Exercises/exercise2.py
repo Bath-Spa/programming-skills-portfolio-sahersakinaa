24efe19cfa6581fad9afbf7bc4839eb221df8d38
sahersakinaa <saher.bathspa@gmail.com>
2024-01-17 20:31:01 +0400

    Create exercise2.py
    Co-Authored-By: Anmol-97 <82800497+Anmol-97@users.noreply.github.com>

---
+++ Chapter 1- Getting Started with Python/Exercises/exercise2.py
@@ -0,0 +1,14 @@
+"""
+Chap 1,Ex2: Print the Version of Python
+
+"""
+
+# Python program to get the Python version.
+
+# The version info and prints both as output and its code import the system's version.
+
+
+import sys
+print("version info")
+print(sys.version_info)
+print("python version") 
