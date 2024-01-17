b3c3a352a783a7056b1725f965dcfd87521a8e58
sahersakinaa <saher.bathspa@gmail.com>
2024-01-18 01:29:25 +0400

    Create excerise2.py
    Co-Authored-By: Anmol-97 <82800497+Anmol-97@users.noreply.github.com>

---
+++ Chapter 7- Functions/Exercises/excerise2.py
@@ -0,0 +1,7 @@
+# Exercise 2: Favorite Book :ballot_box_with_check:
+""
+def favorite_book(title):
+    "write a message about my favorite book."
+    print(f" {title} is  my favorite books.")
+    
+favorite_book('yaadein gungonati hain')
