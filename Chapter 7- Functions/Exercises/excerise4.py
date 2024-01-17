b975c84f5db5fd8a79876f5dc5d352a7e6953845
sahersakinaa <saher.bathspa@gmail.com>
2024-01-18 01:29:47 +0400

    Create excerise4.py
    Co-Authored-By: Anmol-97 <82800497+Anmol-97@users.noreply.github.com>

---
+++ Chapter 7- Functions/Exercises/excerise4.py
@@ -0,0 +1,10 @@
+# Exercise 4:  Large Shirts :ballot_box_with_check:
+""
+def make_shirt(size='XXL', message='I Like Python'):
+    "Summarize the shirt that's going to be made."
+    print(f"\n I'm going to make a {size} t-shirt.")
+    print(f'It will say, "{message}"')
+    
+make_shirt()
+make_shirt(size='XL')
+make_shirt('large','Python is very Fascinating.')
