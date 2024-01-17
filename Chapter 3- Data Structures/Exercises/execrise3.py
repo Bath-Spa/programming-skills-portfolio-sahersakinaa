3b2250d7968fa9c2a9dc365455f5fc07adc447c0
sahersakinaa <saher.bathspa@gmail.com>
2024-01-17 20:48:14 +0400

    Create execrise3.py
    Co-Authored-By: Anmol-97 <82800497+Anmol-97@users.noreply.github.com>

---
+++ Chapter 3- Data Structures/Exercises/execrise3.py
@@ -0,0 +1,18 @@
+"""
+Chapter 3, Exercise 3: Your Own List ☑️
+Think of your favorite mode of transportation, such as a motorcycle or a car, and make a list that stores several examples. Use your list
+
+to print a series of statements about these items, such as “I would like to own a Honda motorcycle.”
+"""
+
+#The varaible 'transport_list' stores a vehicales list. Each vehicale have a statement and printed as a output.
+transport_list=['Motorcycle','Boat','Jeep']
+
+statement=f"I would like to own a Harley Davison{transport_list[0].title()}"
+print(statement)
+
+statement=f"I would like to own a Sailboat{transport_list[0].title()}"
+print(statement)
+
+statement=f"I would like to own a Thar{transport_list[0].title()}"
+print(statement)
