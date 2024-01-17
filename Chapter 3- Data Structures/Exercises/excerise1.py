9e402936153a933817411a45ec1be954a2fc192d
sahersakinaa <saher.bathspa@gmail.com>
2024-01-17 20:47:58 +0400

    Create excerise1.py
    Co-Authored-By: Anmol-97 <82800497+Anmol-97@users.noreply.github.com>

---
+++ Chapter 3- Data Structures/Exercises/excerise1.py
@@ -0,0 +1,13 @@
+"""
+Chapter 2, Exercise 1: Names ☑️
+Store the names of a few of your friends in a list called names. Print each person’s name by accessing each element in the list, one at a
+
+time.
+
+"""
+
+name=['Jaun Elia', 'Ali Zayroun', 'Tehzeeb Hafi']
+
+print(name[0])
+print(name[1])
+print(name[2])
