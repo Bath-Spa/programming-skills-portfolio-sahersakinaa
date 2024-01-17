f9703332c63c5540d9b551cf34d574f8843b0908
sahersakinaa <saher.bathspa@gmail.com>
2024-01-18 01:29:40 +0400

    Create excerise3.py
    Co-Authored-By: Anmol-97 <82800497+Anmol-97@users.noreply.github.com>

---
+++ Chapter 7- Functions/Exercises/excerise3.py
@@ -0,0 +1,9 @@
+# Exercise 3: T-Shirt  :ballot_box_with_check:
+""
+def make_shirt(size, message):
+    "Summarize the shirt that is going to be made."
+    print(f"\nI'm going to make a {size} t-shirt.")
+    print(f'It will say "{message}"')
+    
+make_shirt('XL','remmeber me!')
+make_shirt(message="don't worry i am always there.",size='large')
