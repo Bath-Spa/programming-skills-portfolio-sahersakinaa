ce34887767441a25639f1c38d1cc555443a63f24
sahersakinaa <saher.bathspa@gmail.com>
2024-01-18 01:18:17 +0400

    Create excerise2.py

---
+++ Chapter 5- Dictionaries/Exercises/excerise2.py
@@ -0,0 +1,24 @@
+## Exercise 2: Glossary :ballot_box_with_check:
+""
+glossary={
+    'string':'good as a reaction',
+    'comment':'A not in a python that the python interpreter ignores',
+    'list':'A list of collection ina particular order',
+    'loop':'Work through a collection of items, one at a time',
+    'dictionary':'A collection of key_value pairs',
+    }
+
+word='string'
+print(f"\n{word.title()}:{glossary[word]}")
+
+word='comment'
+print(f"\n{word.title()}:{glossary[word]}")
+
+word='list'
+print(f"\n{word.title()}:{glossary[word]}")
+
+word='loop'
+print(f"\n{word.title()}:{glossary[word]}")
+
+word='dictionary'
+print(f"\n{word.title()}:{glossary[word]}")
