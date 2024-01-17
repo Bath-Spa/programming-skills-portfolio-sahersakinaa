e5609ed84220019ec253dc68f77c16e69ededb6d
sahersakinaa <saher.bathspa@gmail.com>
2024-01-18 01:23:01 +0400

    Create excerise3.py
    Co-Authored-By: Anmol-97 <82800497+Anmol-97@users.noreply.github.com>

---
+++ Chapter 6- Loops/Exercises/excerise3.py
@@ -0,0 +1,8 @@
+# Exercise 3: Infinity :ballot_box_with_check:
+""
+x=0
+# choose a number & store it in a variable.
+while x==0:
+    # Creating a loop which can make the statement on line 3 true
+    print('This loop will never Finish')
+    # The message that prints  the loop
