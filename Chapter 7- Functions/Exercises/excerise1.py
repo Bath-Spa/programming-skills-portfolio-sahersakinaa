86641380a249c84f6d91b2277db5a4dad8acb032
sahersakinaa <saher.bathspa@gmail.com>
2024-01-18 01:29:10 +0400

    Create excerise1.py
    Co-Authored-By: Anmol-97 <82800497+Anmol-97@users.noreply.github.com>

---
+++ Chapter 7- Functions/Exercises/excerise1.py
@@ -0,0 +1,7 @@
+#1:   Message  :ballot_box_with_check:
+""
+def display_message():
+    """Display a message about what I'm learning."""
+    msg= "I'm learning how to store code in functions."
+    print(msg)
+display_message()
