73ed432264308c496ad557b1beea7e7da0256cf6
sahersakinaa <saher.bathspa@gmail.com>
2024-01-18 01:18:11 +0400

    Create excerise1.py

---
+++ Chapter 5- Dictionaries/Exercises/excerise1.py
@@ -0,0 +1,11 @@
+## Exercise 1: Person :ballot_box_with_check:
+""
+person={
+        'first_name':('iman'),
+        'last_name':('safina'),
+        'age':('19'),
+        }
+
+print(person['first_name'])
+print(person['last_name'])
+print(person['age'])
