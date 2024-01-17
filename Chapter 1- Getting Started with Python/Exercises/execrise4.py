6cc8405ad8e6dba3c21fbc021ccfeded639168a1
sahersakinaa <saher.bathspa@gmail.com>
2024-01-17 20:29:40 +0400

    Create execrise4.py
    Co-Authored-By: Anmol-97 <82800497+Anmol-97@users.noreply.github.com>

---
+++ Chapter 1- Getting Started with Python/Exercises/execrise4.py
@@ -0,0 +1,9 @@
+#Exercise 4: Strings Concatination 
+#Write three strings in different variables and print the output as one string.      
+
+#  input is used to input three words and printed altogether as a output.
+
+sword=input("Input your first word")
+tord=input("Input your second word")
+fword=input("Input you third word")
+print(sword , toward , fword)
