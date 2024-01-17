9a17ad0fe4f5ca55ea84293276793680fd727c41
sahersakinaa <saher.bathspa@gmail.com>
2024-01-17 20:41:26 +0400

    Create execrise2.py
    Co-Authored-By: Anmol-97 <82800497+Anmol-97@users.noreply.github.com>

---
+++ Chapter 2- Variables & Comments/Exercises/execrise2.py
@@ -0,0 +1,6 @@
+"""
+Chapter 2, Exercise 2: Variables ☑️
+Find a quote from a famous person you admire. Print the quote and the name of its author.
+"""
+print(' jaun elia said, "kitab e taqdeer sy kiya shikwa aa dil e naddan, jab har cheez ny mit jahna hai tu phir khuwaish kasi."')
+print("kitab e taqdeer sy kiya shikwa aa dil e naddan, jab har cheez ny mit jahna hai tu phir khuwaish kasi")
