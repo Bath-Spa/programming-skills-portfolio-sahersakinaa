b386eb43a8cc6e8592dd0b7b32b2f2f1974f9fb8
sahersakinaa <saher.bathspa@gmail.com>
2024-01-17 20:48:25 +0400

    Create execrise2.py
    Co-Authored-By: Anmol-97 <82800497+Anmol-97@users.noreply.github.com>

---
+++ Chapter 3- Data Structures/Exercises/execrise2.py
@@ -0,0 +1,18 @@
+"""
+Chapter 3, Exercise 2: Greetings ☑️
+Start with the list you used in Exercise 1, but instead of just
+
+printing each person’s name, print a message to them. The text of each message should be the same, but each message should be
+
+personalized with the person’s name.
+"""
+names=['Jaun Elia','Ali Zayroun','Tehzeeb Hafi']
+
+msg=(f" I hope i can listen your poetry face to face. , {names[0].title()}!")
+print(msg)
+
+msg=(f" I hope i can listen your poetry face to face., {names[1].title()}!")
+print(msg)
+
+msg=(f" I hope i can listen your poetry face to face., {names[2].title()}!")
+print(msg)
