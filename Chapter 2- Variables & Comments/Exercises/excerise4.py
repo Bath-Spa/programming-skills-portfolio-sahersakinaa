abadcd064f783362157b5ad71d6e24b08f5767ef
sahersakinaa <saher.bathspa@gmail.com>
2024-01-17 20:42:30 +0400

    Create excerise4.py
    Co-Authored-By: Anmol-97 <82800497+Anmol-97@users.noreply.github.com>

---
+++ Chapter 2- Variables & Comments/Exercises/excerise4.py
@@ -0,0 +1,10 @@
+"""
+Chapter 2, Exercise 4: Favorite Number ☑️
+Use a variable to represent your favorite number. Then,using that variable, create a message that reveals your favorite number. Print that message.
+
+"""
+fav_num=1498
+msg=(f"My faviourt number is {fav_num}")
+
+
+print(msg)
