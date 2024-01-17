81b406c2587dd6cb985dd62a37628e89bbd86e63
sahersakinaa <saher.bathspa@gmail.com>
2024-01-17 20:48:33 +0400

    Create execrise4.py
    Co-Authored-By: Anmol-97 <82800497+Anmol-97@users.noreply.github.com>

---
+++ Chapter 3- Data Structures/Exercises/execrise4.py
@@ -0,0 +1,17 @@
+"""
+Chapter 3,Exercise 4: Guest List ☑️
+If you could invite anyone, living or deceased, to dinner, who would you invite? Make a list that includes at least three people you’d
+
+like to invite to dinner. Then use your list to print a message to each person, invitingthem to dinner.
+"""
+
+guests=['Dawood','paul','aims']
+
+name=guests[0].title()
+print(f"{name}, You are invited to a dinner.")
+
+name=guests[1].title()
+print(f"{name},You are invited to a dinner.")
+
+name=guests[2].title()
+print(f"{name},You are invited to a dinner.")
